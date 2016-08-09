4eac3d4765822a98c7422d2b4ba9398296761ddf
Emanuel Esquivel Canales <Emanuel Esquivel Canales>
2016-08-09 17:27:00 -0600

    conflicto de drag and drop de proyeto

--- js/script.js
+++ js/script.js
@@ -36,6 +36,7 @@ function deletee(){
 });
 }
 /* crea el contairne y demas de los proyectos*/
+
 $('.crearp').click(function(evento){
   var proyecto = $('.texto').val();
   if(proyecto===""){
@@ -49,8 +50,7 @@ $('.crearp').click(function(evento){
     habilitarEdit();
      deletee();
      drag_drop(''+proyecto+'');
-    // $( '.'+proyecto+'' ).draggable({ containment: "#contdrop", scroll: false });
-
+    
    }else{
       alert("Este proyecto ya Exite");
      }
@@ -115,42 +115,7 @@ $('.crearp').click(function(evento){
         }
     });
 
-//     $('drag').click(function(){
-// DragDrop("drag","container");
-//     });
-    
-/*drag and drop*/
 
- //   function DragDrop(){
- //   $("#dp").draggable();
- //   }
- // $('crearP').click(function(e){
-//      DragDrop("drag","drop");
-//     });
-
-// function DragDrop(drag, drop) {
-
-// var drag = document.getElementById(drag);
-// var drop = document.getElementById(drop);
-
-// drag.ondragstart = function(e)
-// {
-// //Guardamos el id del elemento para transferirlo al elemento drop
-// //Contenido es una clave que nos permitirá acceder al valor asignado
-// e.dataTransfer.setData("contenido", e.target.id);
-// }
-
-// drop.ondragover = function(e){
-// //Cancelar el evento que impide que podamos soltar el elemento drag
-// e.preventDefault();
-// }
-
-// drop.ondrop = function(e){
-// //Obtenemos los datos a través de la clave contenido, en este caso el id
-// var id= e.dataTransfer.getData("contenido");
-// e.target.appendChild(document.getElementById(id));
-// }
-// }
 /*realiza el desplazamiento de panel*/
   $('#flip').click(function(){
      $('#panel').slideToggle("slow");
